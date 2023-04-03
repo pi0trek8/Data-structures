f8694b4cf1a07473de18d22cc41c6f8438a273fa
Pi0trek8 <piotrekszczypior1@gmail.com>
2023-04-03 21:25:02 +0200

    [feature] - implementation of methods push and pop in binary heap

--- src/Collections/BinaryHeap/BinaryHeap.cpp
+++ src/Collections/BinaryHeap/BinaryHeap.cpp
@@ -9,6 +9,33 @@
 
 using namespace std;
 
+void BinaryHeap::heapify_down(int index) {
+    int left_child_index = 2 * index + 1;
+    int right_child_index = 2 * index + 2;
+    int largest = index;
+
+    if (right_child_index < _heap.get_size() && _heap.get(right_child_index) > _heap.get(largest)) {
+        largest = right_child_index;
+    }
+    if (left_child_index < _heap.get_size() && _heap.get(left_child_index) > _heap.get(largest)) {
+        largest = left_child_index;
+    }
+
+    if (largest != index) {
+        _heap.swap(index, largest);
+        heapify_down(largest);
+    }
+}
+
+void BinaryHeap::heapify_up(int index) {
+    int parent_index = (index - 1) / 2;
+
+    if (index && _heap.get(index) > _heap.get(parent_index)) {
+        _heap.swap(parent_index, index);
+        heapify_up(parent_index);
+    }
+}
+
 void BinaryHeap::pop_back() {
     if (_heap.get_size() == 0) {
         cout << "Heap is empty" << endl;
@@ -17,9 +44,6 @@ void BinaryHeap::pop_back() {
     _heap.swap(0, _heap.get_size() - 1);
     _heap.pop_back();
     heapify_down(0);
-
-    //todo: after pop_back structure can be bad?
-//    heapify_up(_heap.get_size() - 1);
 }
 
 void BinaryHeap::push_back(int value) {
@@ -27,8 +51,9 @@ void BinaryHeap::push_back(int value) {
     heapify_up(_heap.get_size() - 1);
 }
 
-int BinaryHeap::findElementByIndex() {
-    return 0;
+bool BinaryHeap::find_element() {
+    //TODO: implementation Ku
+    return false;
 }
 
 void BinaryHeap::print() {
@@ -47,33 +72,10 @@ void BinaryHeap::print(std::string prefix, int index, bool isLeft) {
     }
 }
 
-void BinaryHeap::push_front(int element) {
-
-}
-
-void BinaryHeap::insert(int element, int index) {
-
-}
-
-void BinaryHeap::remove(int index) {
-
-}
-
-void BinaryHeap::pop_front() {
-
-}
-
-void BinaryHeap::clear() {
-
-}
-
-int BinaryHeap::find(int element) {
-    return 0;
-}
-
 std::string BinaryHeap::get_name() {
     return "Binary_Heap";
 }
+
 
 
 
--- src/Collections/BinaryHeap/BinaryHeap.h
+++ src/Collections/BinaryHeap/BinaryHeap.h
@@ -7,41 +7,15 @@
 
 
 #include <algorithm>
-#include "../DoubleList/DoubleList.h"
 #include "../Array/Array.h"
 
 class BinaryHeap : AbstractCollection {
 private:
-    DoubleList _heap;
+    Array _heap;
 
-    void heapify_down(int index) {
-        int left_child_index = 2 * index + 1;
-        int right_child_index = 2 * index + 2;
-        int largest = index;
+    void heapify_down(int index);
 
-        if (right_child_index < _heap.get_size() && _heap.get(right_child_index) > _heap.get(largest)) {
-            largest = right_child_index;
-        }
-        if (left_child_index < _heap.get_size() && _heap.get(left_child_index) > _heap.get(largest)) {
-            largest = left_child_index;
-        }
-
-
-
-        if (largest != index) {
-            _heap.swap(index, largest);
-            heapify_down(largest);
-        }
-    }
-
-    void heapify_up(int index) {
-        int parent_index = (index - 1) / 2;
-
-        if(index && _heap.get(index) > _heap.get(parent_index)) {
-            _heap.swap(parent_index, index);
-            heapify_up(parent_index);
-        }
-    }
+    void heapify_up(int index);
 
     void print(std::string prefix, int value, bool isLeft);
 
@@ -51,27 +25,11 @@ public:
 
     void pop_back() override;
 
-    void push_front(int element) override;
-
-    void insert(int element, int index) override;
-
-    void remove(int index) override;
-
-    void pop_front() override;
-
-    void clear() override;
-
-    int find(int element) override;
-
-    int findElementByIndex();
+    bool find_element();
 
     void print();
 
     std::string get_name();
-
-    void printHeap() {
-        _heap.print();
-    }
 };
 
 
